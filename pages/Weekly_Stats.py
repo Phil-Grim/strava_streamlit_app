0a60aa56f9e5856d72c10101828a42b47c849b99
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-27 18:29:04 +0000

    Update Weekly_Stats.py

--- pages/Weekly_Stats.py
+++ pages/Weekly_Stats.py
@@ -1,9 +1,9 @@
 import streamlit
-import Home
+import streamlit_app
 # from streamlit_app import displayText
 # from streamlit_app import strava_json_to_df
 
-Home.displayText()
+streamlit_app.displayText()
 
-Home.strava_json_to_df()
+# streamlit_app.strava_json_to_df()
 # streamlit.dataframe(df)
