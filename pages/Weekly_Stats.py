14585e774ff3943f3ce79460b05b2b9a8705fb46
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-27 23:27:16 +0000

    Update Weekly_Stats.py

--- pages/Weekly_Stats.py
+++ pages/Weekly_Stats.py
@@ -10,5 +10,5 @@ streamlit_app.displayText()
 # try a second super simple function in streamlit_app
 # try putting functions in same directory (i.e. pages)
 
-streamlit_app.strava_json_to_df()
+df = streamlit_app.strava_json_to_df()
 streamlit.dataframe(df)
