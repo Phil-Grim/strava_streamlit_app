ee495ca444ef1f201f0b85a00ea266b72485b4ba
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-28 10:09:15 +0000

    Update Weekly_Stats.py

--- pages/Weekly_Stats.py
+++ pages/Weekly_Stats.py
@@ -30,13 +30,13 @@ current_week['Date'] = current_week['Date'].dt.date
 current_week_summed = current_week.groupby(pd.Grouper(key='Date')).sum().reindex(r).fillna(0.0).rename_axis('Date').reset_index()
 
 csfont = {'fontname':'Avenir'}
-plt.bar(current_week_summed['Date'],current_week_summed['Distance'], color= '#49494f')
+fig = plt.bar(current_week_summed['Date'],current_week_summed['Distance'], color= '#49494f')
 plt.xticks(current_week_summed['Date'], ('M', 'T', 'W', 'T', 'F', 'S', 'S'), **csfont)
 plt.yticks(**csfont)
 plt.tick_params(top=False, bottom=False, left=False, right=False, labelleft=True, labelbottom=True, labelsize=15)
 plt.box(False)
 
-streamlit.plt
+streamlit.plt(fig)
 
 
 
