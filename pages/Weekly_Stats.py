39c3d8c4f51286c9ed8438f22a5128b75306ad6a
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-28 10:11:02 +0000

    Update Weekly_Stats.py

--- pages/Weekly_Stats.py
+++ pages/Weekly_Stats.py
@@ -36,7 +36,7 @@ plt.yticks(**csfont)
 plt.tick_params(top=False, bottom=False, left=False, right=False, labelleft=True, labelbottom=True, labelsize=15)
 plt.box(False)
 
-streamlit.plt(fig)
+streamlit.pyplot(fig)
 
 
 
